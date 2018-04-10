4689f721ccd8eb2ef03fa153ae7677b176a07a9d
heanrum <18990669683@qq.com>
2018-04-10 22:54:23 +0800

    update tinymce

--- app/static/js/tinymce_setup.js
+++ app/static/js/tinymce_setup.js
@@ -1,16 +1,29 @@
 //For submit articles
 tinymce.init({
-    selector: '#content',
+    selector:'#content',
+    codesample_languages: [
+        {text: 'HTML/XML', value: 'markup'},
+        {text: 'JavaScript', value: 'javascript'},
+        {text: 'CSS', value: 'css'},
+        {text: 'PHP', value: 'php'},
+        {text: 'Ruby', value: 'ruby'},
+        {text: 'Python', value: 'python'},
+        {text: 'Java', value: 'java'},
+        {text: 'C', value: 'c'},
+        {text: 'C#', value: 'csharp'},
+        {text: 'C++', value: 'cpp'}
+    ],
     directionality:'ltr',
     language:'zh_CN',
     height:400,
     plugins: [
             'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
-            'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+            'searchreplace wordcount visualblocks visualchars code codesample fullscreen insertdatetime media nonbreaking',
             'save table contextmenu directionality emoticons template paste textcolor',
             'codesample',
     ],
-     toolbar: 'insertfile undo redo | \
+     toolbar: 'insertfile undo redo  | \
+     codesample | \
      styleselect | \
      bold italic | \
      alignleft aligncenter alignright alignjustify | \
